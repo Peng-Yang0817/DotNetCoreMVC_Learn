25310be5bb22b3fe8fe0649a2b754f74e377a29f
tcher2007 <tcher2007>
2023-06-06 00:14:07 +0800

    完成CRUD的模板

--- NoteDotNetCoreUseReverseEngineering/Areas/Auth/Controllers/Auth001Controller.cs
+++ NoteDotNetCoreUseReverseEngineering/Areas/Auth/Controllers/Auth001Controller.cs
@@ -1,12 +1,125 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using NoteDotNetCoreUseReverseEngineering.Models.TQLDB.Tables;
+using Microsoft.EntityFrameworkCore;
+
 
 namespace NoteDotNetCoreUseReverseEngineering.Areas.Auth.Controllers
 {
     public class Auth001Controller : Controller
     {
+        // 初始化DbContext
+        private readonly TQLDBTablesContext dbTable;
+        public Auth001Controller(TQLDBTablesContext dbTable)
+        {
+            this.dbTable = dbTable;
+        }
+
         public IActionResult Index()
         {
             return View();
+        }
+        /// <summary>
+        /// 移除用戶區域
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public IActionResult Auth001Delect()
+        {
+            ViewBag.UserName = "";
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Auth001Delect(string UserName)
+        {
+            List<Auth001> targetDataList_auth001 = dbTable.Auth001s.Where(x => x.Username == UserName).ToList();
+
+            // 符合
+            if (!(targetDataList_auth001.Count == 0 ||
+                targetDataList_auth001.Count > 1))
+            {
+                Auth001 targetData_auth001 = targetDataList_auth001[0];
+                dbTable.Auth001s.Remove(targetData_auth001);
+                dbTable.SaveChanges();
+
+                return RedirectToAction("Index", "Home", new { area = "" });
+            }
+            ViewBag.UserName = UserName;
+            return View();
+        }
+        /// <summary>
+        /// 用戶更新密碼區
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public IActionResult Auth001UpdatePassword()
+        {
+            ViewBag.UserName = "";
+            ViewBag.password_old = "";
+            ViewBag.password_new = "";
+            ViewBag.Email = "";
+            ViewBag.ErroorMassages = new List<string>();
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Auth001UpdatePassword(string UserName, string Email, string password_old, string password_new)
+        {
+            List<Auth001> targetDataList_auth001 = dbTable.Auth001s.Where(x => x.Username == UserName &&
+                                                                                x.Email == Email &&
+                                                                                x.Password == password_old).ToList();
+            // 符合
+            if (targetDataList_auth001.Count == 1)
+            {
+                Auth001 target_auth001 = targetDataList_auth001[0];
+                target_auth001.Password = password_new;
+                dbTable.Entry(target_auth001).State = EntityState.Modified;
+                dbTable.SaveChanges();
+                return RedirectToAction("Index", "Home", new { area = "" });
+            }
+            List<string> ErroorMassages = new List<string>();
+            // 不符合
+            if (targetDataList_auth001.Count == 0)
+            {
+                ErroorMassages.Add("查無用戶資料");
+            }
+            if (targetDataList_auth001.Count > 1)
+            {
+                ErroorMassages.Add("系統問題! 多使用者狀況! 請聯繫系統管理者。");
+            }
+            ViewBag.UserName = UserName;
+            ViewBag.password_old = password_old;
+            ViewBag.password_new = password_new;
+            ViewBag.Email = Email;
+            ViewBag.ErroorMassages = ErroorMassages;
+            return View();
+        }
+
+        /// <summary>
+        /// 新增用戶區域
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public IActionResult Auth001Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Auth001Create(Auth001 get_Auth001)
+        {
+            // Model is valid
+            if (ModelState.IsValid)
+            {
+                dbTable.Add(get_Auth001);
+                dbTable.SaveChanges();
+
+                // return RedirectToAction("Index", "Home", new { area = "AnotherArea", a = a });
+                return RedirectToAction("Index", "Home", new { area = "" });
+            }
+            // Model is not valid
+            return View(get_Auth001);
         }
     }
 }
--- NoteDotNetCoreUseReverseEngineering/Models/TQLDB/Tables/Auth001.cs
+++ NoteDotNetCoreUseReverseEngineering/Models/TQLDB/Tables/Auth001.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace NoteDotNetCoreUseReverseEngineering.Models.TQLDB.Tables;
 
@@ -13,61 +14,73 @@ public partial class Auth001
     /// <summary>
     /// 使用者帳號
     /// </summary>
+    [Display(Name = "使用者帳號")]
     public string Username { get; set; } = null!;
 
     /// <summary>
     /// 使用者密碼
     /// </summary>
+    [Display(Name = "使用者密碼")]
     public string Password { get; set; } = null!;
 
     /// <summary>
     /// 使用者 E-Mail
     /// </summary>
+    [Display(Name = "使用者 E-Mail")]
     public string Email { get; set; } = null!;
 
     /// <summary>
     /// 使用者姓名
     /// </summary>
+    [Display(Name = "使用者姓名")]
     public string Name { get; set; } = null!;
 
     /// <summary>
     /// SysCtrl002 roleRange
     /// </summary>
+    [Display(Name = "使用者權限")]
     public string RoleRange { get; set; } = null!;
 
     /// <summary>
     /// SchoolCtrl001 FK
     /// </summary>
+    [Display(Name = "SchoolCtrl001 FK")]
     public int? SchoolCtrl001Id { get; set; }
 
     /// <summary>
     /// 使用者單位
     /// </summary>
+    [Display(Name = "使用者單位")]
     public string? Office { get; set; }
 
     /// <summary>
     /// 使用者電話
     /// </summary>
+    [Display(Name = "使用者電話")]
     public string? Phone { get; set; }
 
     /// <summary>
     /// 使用者手機
     /// </summary>
+    [Display(Name = "使用者手機")]
     public string? Cellphone { get; set; }
 
     /// <summary>
     /// 使用者傳真號碼
     /// </summary>
+    [Display(Name = "使用者傳真號碼")]
     public string? Fax { get; set; }
 
     /// <summary>
     /// 使用者狀態(開啟,關閉)
     /// </summary>
+    [Display(Name = "使用者狀態(開啟,關閉)")]
     public int State006Id { get; set; }
 
     /// <summary>
     /// 使用者建立時間
     /// </summary>
+    [Display(Name = "使用者建立時間")]
     public DateTime CreateTime { get; set; }
 
     public bool IsChangeInfo { get; set; }
@@ -75,6 +88,7 @@ public partial class Auth001
     /// <summary>
     /// 審查委員負責之送審領域
     /// </summary>
+    [Display(Name = "審查委員負責之送審領域")]
     public int? State013Id { get; set; }
 
     public string? JobTitle { get; set; }
@@ -85,9 +99,19 @@ public partial class Auth001
 
     public string? Address { get; set; }
 
+    /// <summary>
+    /// 辦公處所
+    /// </summary>
+    [Display(Name = "辦公處所")]
     public string? DepartmentName { get; set; }
-
+    /// <summary>
+    /// 辦公處所
+    /// </summary>
+    [Display(Name = "是否開啟狀態")]
     public bool? IsChangePassword { get; set; }
-
+    /// <summary>
+    /// 辦公處所
+    /// </summary>
+    [Display(Name = "是否開啟使用")]
     public bool? IsEncrypted { get; set; }
 }
